a36aa5db14dc379d65059fad4396c774afa8a7d7
cosmin_bucur <cosmin.bucur@orange.com>
2019-08-09 16:14:30 +0300

    initial commit

---
+++ src/test/java/com/sda/regex/DependencyParserTest.java
@@ -0,0 +1,16 @@
+package com.sda.regex;
+
+import org.junit.jupiter.api.Test;
+
+class DependencyParserTest {
+
+    // TODO: implement this
+    @Test
+    void givenLongString_whenConvertToShortForm_thenReturnShortString() {
+        // given
+
+        // when
+
+        // then
+    }
+}
